bb5a990e91477f0bfe8277e8871e238084ffe9e1
ayusuningsih <ayuusuningsih20@gmail.com>
2024-02-06 19:57:51 +0700

    day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,16 @@
+//WPU CODING CHALLENGE 2024
+//DAY 10/366
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript
+
+/*
+
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+*/
+
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("ayu suningsih"));
