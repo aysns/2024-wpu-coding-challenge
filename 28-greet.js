27d49a7702474fa82344f5a53c90017f995a622b
ayusuningsih <ayuusuningsih20@gmail.com>
2024-02-22 13:03:15 +0700

    day 18/366

---
+++ 28-greet.js
@@ -0,0 +1,23 @@
+//WPU CODING CHALLENGE 2024
+//DAY 18/366
+//https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
+
+/*
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+// function greet (name, owner) {
+//   // Add code here
+// }
+
+const greet = (name,owner) => name.toLowerCase() === owner.toLowerCase() ? "Hello boss" : "Hello guest";
+
+console.log(greet('daniel', 'Daniel'));
